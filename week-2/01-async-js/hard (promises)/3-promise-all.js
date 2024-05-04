9ae48c3bd0a5a44762104e22d4a7c476bcdf7fe6
Ranita Saha <saharanitaa1230@gmail.com>
2024-05-04 20:24:08 +0530

    feat:added promises

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,33 +5,35 @@
  */
 
 function wait1(t) {
-    return new Promise((resolve,reject)=>{
-        setTimeout(resolve,t);
-    })
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, t*1000);
+    });
 }
 
 function wait2(t) {
-    return new Promise((resolve,reject)=>{
-        setTimeout(resolve,t);
-    })
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, t*1000);
+    });
 }
 
 function wait3(t) {
-    return new Promise((resolve,reject)=>{
-        setTimeout(resolve,t);
-    })
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(), t*1000);
+    });
 }
 
 function calculateTime(t1, t2, t3) {
-    const start=Date.now();
+    const start = Date.now();
 
-    return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then(()=>{
-        const end=Date.now();
-        return end-start;
-    })
+    return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then(() => {
+        const end = Date.now();
+        console.log('Start:', start, 'End:', end);
+        return end - start;
+    });
 }
 
-calculateTime(1000, 2000, 3000).then((time) => {
+
+calculateTime(10,1,1).then((time) => {
     console.log("Total time:", time, "milliseconds");
 });
 
--- week-2/01-async-js/tests/4-promise-chain.test.js
+++ week-2/01-async-js/tests/4-promise-chain.test.js
@@ -4,18 +4,18 @@ describe("calculateTime function", () => {
   test("calculates time 1", async () => {
     const difference = await calculateTime(1, 2, 3);
     expect(difference).toBeGreaterThanOrEqual(6000);
-    expect(difference).toBeLessThan(7000);
+    expect(difference).toBeLessThan(7000);//6026
   }, 7000);
 
   test("calculates time 2", async () => {
     const difference = await calculateTime(10, 1, 1);
     expect(difference).toBeGreaterThanOrEqual(12000);
-    expect(difference).toBeLessThan(13000);
+    expect(difference).toBeLessThan(13000);//12039
   }, 14000);
 
   test("calculates time for zero seconds", async () => {
     const difference = await calculateTime(0, 0, 0);
     expect(difference).toBeGreaterThanOrEqual(0);
-    expect(difference).toBeLessThan(100);
+    expect(difference).toBeLessThan(100);//37
   }, 100);
 });
