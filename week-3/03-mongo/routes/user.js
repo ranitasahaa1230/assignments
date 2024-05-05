1f62f5a1490ebadce5c1a8c26e5fb63a2354ad25
Ranita Saha <saharanitaa1230@gmail.com>
2024-05-05 14:33:48 +0530

    feat:added mongo

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -15,20 +15,49 @@ router.post("/signup", async (req, res) => {
 
 router.get("/courses", async (req, res) => {
   // Implement listing all courses logic
-  const response = await Course.find({});
-  res.json({ courses: response });
+
+  try {
+    const response = await Course.find({});
+    res.json({ courses: response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-router.post("/courses/:courseId", userMiddleware, async(req, res) => {
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
   // Implement course purchase logic
-  
-  const {courseId}=req.params;
- const purchasedCourse=await  Course.findOne();
- res.json({ message: 'Course purchased successfully' })
+
+  const courseId = req.params.courseId;
+  const username = req.headers.username;
+
+  try {
+    await User.updateOne(
+      { username },
+      {
+        "$push": {
+          purchasedCourses: courseId,
+        },
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
+  res.json({ message: "Course purchased successfully" });
 });
 
-router.get("/purchasedCourses", userMiddleware, (req, res) => {
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
   // Implement fetching purchased courses logic
+  const user = await User.findOne({
+    username: req.headers.username,
+  });
+  console.log(user.purchasedCourses);
+  const courses = await Course.find({
+    _id: {
+      $in: user.purchasedCourses,
+    },
+  });
+  res.json({ purchasedCourses: courses });
 });
 
 module.exports = router;
